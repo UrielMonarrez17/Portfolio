15c0ab4f7c806c563cce3531f984f5f4bc9b27d7
Uriel Monarrez <0263097@up.edu.mx>
2024-11-29 13:13:01 -0600

    message :D

--- Portfolio/13-ExpressAuthentication/server.js
+++ Portfolio/13-ExpressAuthentication/server.js
@@ -2,15 +2,33 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const mongoUrl = "mongodb://127.0.0.1:27017/LOTR";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoUrl, { 
+  useNewUrlParser: true,
+   useUnifiedTopology: true 
+  });
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URI }),
+  })
+);
 
 // Definition of a schema
 const userSchema = new mongoose.Schema({
@@ -20,7 +38,84 @@ const userSchema = new mongoose.Schema({
 });
 userSchema.set("strictQuery", true);
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model("User", userSchema);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: '/auth/google/secrets',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const user = await User.findOne({ googleId: profile.id });
+      if (user) return done(null, user);
+
+      const newUser = await User.create({
+        username: profile.displayName,
+        googleId: profile.id,
+      });
+      done(null, newUser);
+    }
+  )
+);
+
+app.get('/register', (req, res) => res.render('register'));
+app.post('/register', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await User.register({ username, email }, password);
+    req.login(user, (err) => {
+      if (err) return next(err);
+      res.redirect('/secrets');
+    });
+  } catch (err) {
+    console.error(err);
+    res.redirect('/register');
+  }
+});
+
+// Login
+app.get('/login', (req, res) => res.render('login'));
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/secrets',
+  failureRedirect: '/login',
+}));
+
+// Secrets
+app.get('/secrets', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render('secrets');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+// Logout
+app.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) console.error(err);
+    res.redirect('/');
+  });
+});
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get(
+  '/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/secrets');
+  }
+);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/html/index.html");
